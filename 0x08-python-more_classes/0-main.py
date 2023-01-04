3ea90eb04da0b142cd31541b16e7b59db16406a7
richtea881 <doctorskull.z@gmail.com>
2023-01-04 03:56:22 +0100

    fix

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+"""
+Defines an empty class Rectangle
+"""
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+
+class Rectangle:
+    """Empty representation of a rectangle"""
+    pass
